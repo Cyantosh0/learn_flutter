fbd899cf7367062b5baeff448ac2794dd2373642
Cyantosh0 <shresthasantosh799@gmail.com>
2021-09-08 08:40:50 +0545

    Add stylings to Tab

--- lib/src/screens/home/widgets/catagories.dart
+++ lib/src/screens/home/widgets/catagories.dart
@@ -4,21 +4,19 @@ class Catagories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 200,
       width: double.infinity,
       child: DefaultTabController(
         length: 3,
         child: Scaffold(
-          appBar: AppBar(
-              bottom: new TabBar(
-            tabs: [
-              Tab(text: "Recommendation"),
-              Tab(text: "Near You"),
-              Tab(text: "Recently Added"),
-            ],
-          )),
+          backgroundColor: Colors.lightBlue[200],
+          appBar: new TabBar(labelColor: Colors.black, tabs: [
+            MyTab(title: 'Recommendation'),
+            MyTab(title: 'Near You'),
+            MyTab(title: 'Recently Added')
+          ]),
           body: TabBarView(
-            children: [Recommendations(), Recommendations(), Recommendations()],
+            children: [Recommendations(), NearYou(), RecentlyAdded()],
           ),
         ),
       ),
@@ -26,11 +24,47 @@ class Catagories extends StatelessWidget {
   }
 }
 
+class MyTab extends StatelessWidget {
+  final String title;
+  MyTab({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+        child: Text(this.title,
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18)));
+  }
+}
+
 class Recommendations extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.cyan[50],
+      height: 50,
       child: Text('This is recommendation section'),
+    );
+  }
+}
+
+class NearYou extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.cyan[50],
+      height: 50,
+      child: Text('This is near you section'),
+    );
+  }
+}
+
+class RecentlyAdded extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.cyan[50],
+      height: 50,
+      child: Text('This is recently added section'),
     );
   }
 }
